2c3e434634a378bde79676c2ba496546166490f4
Abhishek Uppar <abhisheku@iisc.ac.in>
2023-10-16 20:22:09 +0530

    2 Public Test Cases added

--- target1-pub/BasicTest.java
+++ target1-pub/BasicTest.java
@@ -4,8 +4,25 @@ public class BasicTest
 
     static void fun1()
     {
-        BasicTest v1 = new BasicTest();   
-        BasicTest v2 = new BasicTest();   
-        v2.f = v1;                      
+        BasicTest v1 = new BasicTest();
+        BasicTest v2 = new BasicTest();
+        v2.f = v1;
+    }
+
+    static void fun2(int i)
+    {
+        Object t1 = null;
+        Object t2 = new BasicTest();
+        Object t3 = null;
+        if (i>10) {
+            t3 = t1;
+        } else {
+            t3 = t2;
+        }
+        t3.toString();
+    }
+
+    public static void main(String args[])
+    {
     }
 }
