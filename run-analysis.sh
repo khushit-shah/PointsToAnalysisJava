e5180e69daf39f04e466f83d3e9a280496aef353
alokamgnaneswarasai <alokamgnaneswarasai@gmail.com>
2023-11-01 13:25:37 +0530

    added_test_case

--- run-analysis.sh
+++ run-analysis.sh
@@ -13,7 +13,7 @@ set -e
 # ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "fun1"
 # ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "fun2"
 # ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "fun3"
-# ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "circular_references"
+
 # ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "fun5"
 # ./run-analysis-one.sh "./target1-pub" "BasicTest"   "BasicTest"   "fun6"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "assign_check"
@@ -24,13 +24,10 @@ set -e
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "assigning_object_to_field"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "object_comparision"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "empty_function_check"
-# ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "empty_function_check"
-./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "conditional_check_bothNULL"
-# ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "conditional_check5"
+# ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "conditional_check_bothNULL"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "non_static_function"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "implict_type_conversion"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "chained_assignments"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "compare_two_null_values"
-# ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "nullable_object"
 # ./run-analysis-one.sh "./target2-mine" "MyTest"   "MyTest"   "nullable_object"
 
