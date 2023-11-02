1242798ed66e0b57ca6dbe87857336ce39e277ea
Khushit Shah <khushitshah1@gmail.com>
2023-11-03 03:35:04 +0530

    Better and simpler getSimplifiedVarName function

--- Helper.java
+++ Helper.java
@@ -1,21 +1,23 @@
+import soot.Local;
+import soot.Value;
+import soot.jimple.CastExpr;
+import soot.jimple.InstanceFieldRef;
+
 public class Helper {
 
     /**
-     * given a or a.<class: type f>
-     * to,   a or a.f
-     *
-     * @param string
+     * @param Value
      * @return
      */
-    public static String getSimplifiedVarName(String string) {
-        // a or a.<class: type f>
-        if (string.contains(".")) {
-            String[] splits1 = string.split("\\.");
-            String[] splits2 = splits1[splits1.length - 1].split(" ");
-
-            return splits1[0] + "." + splits2[splits2.length - 1].substring(0, splits2[splits2.length - 1].length() - 1);
+    public static String getSimplifiedVarName(Value var) {
+        if (var instanceof Local) {
+            return ((Local) var).getName();
+        } else if (var instanceof CastExpr) {
+            return getSimplifiedVarName(((CastExpr) var).getOp());
+        } else if (var instanceof InstanceFieldRef) {
+            return getSimplifiedVarName(((InstanceFieldRef) var).getBase()) + "." + ((InstanceFieldRef) var).getFieldRef().name();
         } else {
-            return string;
+            return "null";
         }
     }
 
